fc13bf52e67b617af2d01e0cc81715c50f8b631c
kiborgok <borgokalek14@gmail.com>
2018-11-15 19:10:41 +0300

    Removed object created

--- car.py
+++ car.py
@@ -25,32 +25,3 @@ class Car:
         else:
             self.type = 'saloon'
             return self.type
-
-    def drive(self, m_speed):
-        return m_speed
-
-    def car_speed(self):
-        moving_speed = self.drive(7)
-        return moving_speed
-    
-
-if __name__ == '__main__':
-    honda = Car()
-    truck = Car('Truck', 'Isuzu', 'trailer')
-    toyota = Car('Toyota', 'Cruiser')
-    koenigsegg = Car('Koenigsegg', 'model 65')
-    porshe = Car('Porshe', '911 Turbo')
-    man = Car('MAN', 'Truck', 'trailer')
-
-    print('######Wheels######')
-    print(truck.num_of_wheels())
-    print(toyota.num_of_wheels())
-
-    print('######Doors######')
-    print(truck.num_of_doors())
-    print(porshe.num_of_doors())
-    print(koenigsegg.num_of_doors())
-
-    print('######saloon or trailer')
-    print(porshe.is_saloon())
-    print(man.speed)
